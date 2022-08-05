04cc4aac71cd51b85b8f680980c6982332a1db82
yatinbichala <110629503+yatinbichala@users.noreply.github.com>
2022-08-04 21:39:38 -0400

    ghost

---
+++ alljavascript.js
@@ -0,0 +1,6 @@
+<h1> Hi guys this is my own website for free </h1>
+	<p> what should I put here </p>
+	<br>
+	<p> mandy but she's also shorter than me
+	    Sophie is cooler but still shorter
+	    Ava is cool in general </p>
